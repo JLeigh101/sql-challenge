8597c17401dc3505e286f8c71cf1ea70a0844529
JLeigh101 <jonnyleigh123@gmail.com>
2023-03-02 13:41:34 -0600

    finished all queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -27,14 +27,29 @@ INNER JOIN departments AS d ON d.dept_no = de.dept_no;
 
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
-
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules'
 AND LEFT(last_name, 1) = 'B';
 
 --List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT DISTINCT e.emp_no, e.last_name, e.first_name, de.dept_no
+FROM employees AS e
+INNER JOIN dept_emp as de ON de.emp_no = e.emp_no
+INNER JOIN departments as d ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales'
+ORDER BY e.emp_no;
 
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT DISTINCT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp as de ON de.emp_no = e.emp_no
+INNER JOIN departments as d ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+ORDER BY e.emp_no;
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT e.last_name, COUNT(last_name)
+FROM employees AS e
+GROUP BY e.last_name
+ORDER BY COUNT(last_name) DESC;
