16a7832fc7567b66b999b997d452712cd14316f6
JLeigh101 <jonnyleigh123@gmail.com>
2023-03-02 11:35:01 -0600

    completed Hercules B section

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -11,13 +11,6 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
---SELECT i.inventory_id, i.film_id, i.store_id
---FROM inventory i
---JOIN film f
---ON (i.film_id = f.film_id)
---WHERE f.title = 'EARLY HOME';
-
-
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT e.first_name, e.last_name, dm.emp_no, d.dept_name, d.dept_no
 FROM employees as e
@@ -27,12 +20,18 @@ INNER JOIN departments as d on d.dept_no = dm.dept_no;
 --List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 
 SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees as e
-INNER JOIN dept_emp as de ON de.emp_no = e.emp_no
-INNER JOIN departments as d ON d.dept_no = de.dept_no;
+FROM employees AS e
+INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
+INNER JOIN departments AS d ON d.dept_no = de.dept_no;
 
 
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND LEFT(last_name, 1) = 'B';
 
 --List each employee in the Sales department, including their employee number, last name, and first name.
 
